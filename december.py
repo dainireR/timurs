f8bcd4d56bfa080dfaa5f00038ef0b5957a3db2b
timursbirums-ctrl <timursbirums@gmail.com>
2026-01-27 08:23:43 +0200

    Add files via upload

---
+++ december.py
@@ -0,0 +1,250 @@
+import math
+'''
+zime=input("izvelies :), :( ")
+def convert(zime):
+   if zime==":)":
+       print("😊")
+
+   elif zime==":(":
+       print("😒")
+
+    elif zime == ":/":
+       print("🤨")
+
+    elif zime == ":0":
+       print("😮")
+
+    elif zime == "B)":
+       print("😎")
+
+   else:
+       print("💩")
+
+convert(zime)
+
+for k in range (1,11):
+        print(k)
+
+for i in range(20,0,-1):
+   print(i)
+
+for x in range (1,10):
+    y=x**2+3*x-2
+
+print(f"ja x={x}, tad y={y}"  )
+
+
+import random
+
+x=random.randint(0,8)
+print(x)
+
+#A
+import random
+
+m=int(input("m: "))
+
+for i in range(m):
+    sk=random.randint(0,60)
+    print(i+1,". ",sk)
+
+
+
+#b
+import random
+
+m=int(input("m,0,-1"))
+
+for i in range(m):
+    sk=random.randint(0,60)
+    print(i,". ",sk)
+
+c=int(input("sk skaits: "))
+summa=0
+for k in range(c):
+    sk=int(input("sk: "))
+    summa+=sk
+    print(summa)
+print("sum", summa)
+
+
+
+for n in range(7,13):
+    print(n**2)
+
+
+#kvadrat sakne
+for k in range(27,18,-1):
+    print(math.sqrt(k))
+
+print(k**0.5)
+
+
+for i in range(1,50):
+    if i%3==0 or i%5==0:
+        print(i)
+    else:
+        print(f"{i} nedalas ar 3 un 5")
+
+
+n=int(input("n= "))
+if n>1:
+    for i in range(1, n):
+        print(i, i ** 2)
+else:
+    for i in range(n,1):
+        print(i, i ** 2)
+
+for k in range (1,20):
+    if k%2==0:
+        continue
+    else:
+        print(k,"ir nepara")
+
+n=int(input("n: "))
+sum=0
+for i in range(n):
+    sk=int(input("sk: "))
+    if sk>=0:
+        sum+=sk
+    else:
+        break #apstadina ciklu
+print("pozitivo skatu summa ",sum)
+
+n=int(input("n: "))
+    for i in range (n)
+    if
+    else:
+         n%3==0 or n%5==0
+         break
+#1lats=1,42eiro
+lats=1,42
+latu_sk=float(input("ievadi latu apjomu "))
+def pelmenis(lats,latu_sk):
+    print(f'{lats*latu_sk} eur')
+
+    pelemenis()
+'''
+'''''
+def a(kola,summa)
+    while summa<=50:
+        nauda=int(input("naudas apjoms"))
+
+#2026
+#1
+v=input("ievadi virkni: ")
+print(len(v))
+#2
+for i in range(len(v)):
+    print(v[i])
+#3
+print(v[0],v[len(v)-1])#v[-1]
+#4
+v2=input("ievadi 2.virkni: ")
+print(v[0]+"."+v2[0]+".")
+print(v[0],v2[0],sep=".",end=".")
+#5
+print("\n",v.upper())
+#6
+print(v2.lower())
+print(v2.casefold())
+#7
+#ca
+print(v2.casefold())
+#7
+#camelCase
+print(v.casefold()+v2.capitalize())
+#snake_cace
+print(v.casefold()+"_"+v2.lower())
+#8
+#v
+v3=""
+for i in range(len(v)):
+    if v[i]=="a" or v[i]=="e" or v[i]=="o" or v[i]=="i" or v[i]=="u":
+        v3+=""
+    else:
+        v3+=v[i]
+print(v3)
+#9
+s=input("ievadi virkni: ")
+print(s.replace(" ","..."))
+print(s)
+
+#1
+v="thequickdrownfoxjumsoverthelazydog"
+print(v[::2])#katru otro
+#b)
+print(v[::-1])#preteja viekne
+#c)
+print(v[::-2])#katrs otrs preteja virzienā
+#d)
+print(v.count("the"))
+#e)
+print(v.replace("a","d"))
+
+#2
+S=input("ievadi virkni: ")
+def nosaukums():
+    '''
+'''
+
+#1
+v=input("ievadi virkni: ")
+
+print(len(v))
+
+#2
+v2=input("ievadi 2.virkni: ")
+print(v+"%"+v2)
+
+#3
+
+v="logaritms"
+a=(v[::2])
+print(a[::-1])
+
+#4
+v4="zemene"
+
+print(v4.count("e"))
+
+#5
+t="kvadratsakne"
+t1=('')
+t1=t[7::]
+print(t1)
+#6
+t="abrakadabra"
+t=t.
+
+
+#7
+v=input("V")
+burts=input("burts")
+
+if burts in v:
+    print(ord(burts))
+
+#8
+kripto=''
+b=input("ievadi virki: ")
+for i in range(len(v)):
+    x=bin(ord(v)[i])
+    kripto+=x.relpace(__old"b",__new" ")
+print(kripto)
+
+'''
+v=input("ivadi virkni: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
