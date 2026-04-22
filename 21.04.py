80f58a6dabcddcb2b54da5db8bca8d24c2705fb6
dainireR <101051579+dainireR@users.noreply.github.com>
2026-04-22 11:47:27 +0300

    Update 21.04.py
    Kļūdu apskats

--- 21.04.py
+++ 21.04.py
@@ -1,13 +1,14 @@
 import json
+#nav importēts csv!
 valstis=[]
 lauksaimniecubas_ipatsvars=[]
 with open("uzd1.json",encoding="utf-8") as datne:
-
+#nav nolasīti dati, tapēc dati ir tukšums
 for vardnica in dati:
     if vardnica ["nosaukums"]:
     print(["nosaukums"])
 
-print(f'viss aizņem: '["mezs"]+["lauksaimniecibas_zeme"]+["cita_veida_zeme"])
+print(f'viss aizņem: '["mezs"]+["lauksaimniecibas_zeme"]+["cita_veida_zeme"]) #netiek pievienots sarakstam
 
-jauna_vardnica=valstis+lauksaimniecubas_ipatsvars
-as open("rezultati.csv","\t",encoding="utf-8")
+jauna_vardnica=valstis+lauksaimniecubas_ipatsvars #šādi nevar apvienot vārdnīcā
+as open("rezultati.csv","\t",encoding="utf-8") #sintakse šādi neveram datni vaļā, nav norādīts režīms, delimiter liekam citā rindā
