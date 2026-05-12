454728e2cc29e08e29c693a6fb67395101bcf6a6
dainireR <101051579+dainireR@users.noreply.github.com>
2026-05-12 17:00:44 +0300

    Update SQL_speles_Timurs.sql
    labojums

--- SQL_speles_Timurs.sql
+++ SQL_speles_Timurs.sql
@@ -1,7 +1,7 @@
 insert or ignore into inventars values ("S_003","cs:go 2","PC","šautene",16,2020,0);
 insert or ignore into inventars values ("S_004","brawl stars","mobile","fantastika",3,2017,0);
-
-select sum(cena)
+/* aizmirsi turpinājumu from table un ; beigās*/
+select sum(cena) /* from inventars */;
 select max(cena)
 select avg(vecuma_ierobezojums)
 select max(vecuma_ierobezojums)
