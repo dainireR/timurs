d22e24a24e5efd90ce249edbc178019e40117e60
timursbirums-ctrl <timursbirums@gmail.com>
2026-05-12 10:19:20 +0300

    Add files via upload

---
+++ SQL_speles_Timurs.sql
@@ -0,0 +1,8 @@
+insert or ignore into inventars values ("S_003","cs:go 2","PC","šautene",16,2020,0);
+insert or ignore into inventars values ("S_004","brawl stars","mobile","fantastika",3,2017,0);
+
+select sum(cena)
+select max(cena)
+select avg(vecuma_ierobezojums)
+select max(vecuma_ierobezojums)
+select nosaukums,platforma,cena from inventars
